6ed74fcd09f88f66ab5f7b9ef8eb56f795e5762a
Amar Prasad <amarprasad77354@gmail.com>
2024-01-14 18:47:15 +0530

    hero-landing-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,15 @@ import './home.css';
 export default function Home() {
   return (
     <main className="mainContainer">
-        <h1>Main page</h1>
-        <p>Paragraph element</p>
+        <div className="hero">
+          <div className='title'>
+            Your one stop solution to hiring 
+          </div>
+          <div className="subtitle">
+            Elevate your creative projects with our specialized hiring platform designed exclusively for content creators. Unleash the full potential of your creativity by assembling the perfect team for your vision.  
+          </div>
+        </div>
+        <video src={require('../../public/assets/videos/landing-video.mp4')} autoPlay muted loop className='video' />
     </main>
   )
 }
