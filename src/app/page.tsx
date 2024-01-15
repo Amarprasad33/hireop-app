3133ade05021d8b0bef49aa9eed3cbe37d2f660e
Amar Prasad <amarprasad77354@gmail.com>
2024-01-16 00:00:35 +0530

    Section-3-landing-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,9 @@ export default function Home() {
               </div>
             </div>
           </div>
+          <div className="separation"></div>
         </div>
+        
     </main>
   )
 }
